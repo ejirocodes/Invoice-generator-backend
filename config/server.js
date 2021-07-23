d0d6a33c4e0bd88bbba4e58dcfed3c2d49c61b17
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-07-23 06:43:02 +0100

    add content typ, update permissions

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '47d4152971080b688501ca3d5a08340e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '321bab5b-7339-4d83-9fe2-c2970563a26a'
+};
